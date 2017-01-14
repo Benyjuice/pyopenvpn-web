97ee1013d34dfce1ef11cce15e30fef10a282b99
Benyjuice <fuxixi1991@163.com>
2017-01-14 19:33:15 +0800

    add user fecture

---
+++ app/admin/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+class AddUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(1,48)])
+    password = StringField('Password' ,validators=[DataRequired(), Length(4)])
+    role_id = IntegerField('Role ID', validators=[DataRequired()])
+    submit = SubmitField('Add')
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,11 @@
 from . import admin
+from .forms import AddUserForm
 from ..decorator import admin_required
 from .utils import utils
 from flask import render_template
+from ..models import UserModel
+from app import db
+from flask import flash
 
 @admin.route('/')
 @admin_required
@@ -10,3 +14,20 @@ def index():
     users = tool.get_user()
     print(users)
     return render_template('admin/index.html', users=users)
+
+@admin.route('/add_user', methods=['GET','POST'])
+@admin_required
+def add_user():
+    form = AddUserForm()
+
+    if form.validate_on_submit():
+        if UserModel.query.filter_by(username=form.username.data).first():
+            flash('User: %s already exist.' % form.username.data)
+            return render_template('admin/add_user.html', form=form)
+
+        user = UserModel(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('User: %s add successful, now can login via openvpn or web' % user.username)
+
+    return render_template('admin/add_user.html', form=AddUserForm())
